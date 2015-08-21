28e6218c6aae2d1f12cf61adc42276d8772cfe5f
Ashish Ashok <ashishkumar.ashok@echostar.com>
2015-08-21 11:16:32 -0600

    HP-1 Added gitignore and FileToSingleLine

---
+++ src/main/java/FileToSingleLineString.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class FileToSingleLineString {
+
+	public static void main(String args[]) {
+		
+		FileReader fr;
+		try {
+			fr = new FileReader("/Users/ashishkumar.ashok/Projects/Bell/bugzilla/6401/AIHIV/AIHIV.json");
+			BufferedReader br = new BufferedReader(fr);
+			StringBuilder sb = new StringBuilder();
+			String line = "";
+			while ((line=br.readLine())!=null) {
+				
+				sb.append(line.trim());
+			}
+			System.out.println(sb.toString());
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
